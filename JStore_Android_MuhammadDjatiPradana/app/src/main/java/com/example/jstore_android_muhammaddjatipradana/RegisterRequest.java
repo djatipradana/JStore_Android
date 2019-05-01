2605501e29aea164055fa3a94f5d13f06715c366
djatipradana <47686382+djatipradana@users.noreply.github.com>
2019-05-02 02:11:46 +0700

    Modul 9 TP

---
+++ JStore_Android_MuhammadDjatiPradana/app/src/main/java/com/example/jstore_android_muhammaddjatipradana/RegisterRequest.java
@@ -0,0 +1,29 @@
+package com.example.jstore_android_muhammaddjatipradana;
+
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.Response;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RegisterRequest extends StringRequest {
+    private static final String Regis_URL = "http://192.168.1.7:8080/newcustomer";
+    private Map<String, String> params;
+
+    public RegisterRequest(String name, String email, String password, Response.Listener<String> listener){
+        super(Method.POST, Regis_URL, listener, null);
+        params = new HashMap<>();
+        params.put("name", name);
+        params.put("email", email);
+        params.put("username", "defaultUsername");
+        params.put("password", password);
+        params.put("year", "2019");
+    }
+
+    @Override
+    public Map<String, String> getParams(){
+        return params;
+    }
+}
+
+
